805bcfd330b3d6d442703847118b8640bf405a08
WilliamPottey <wottey9007@gmail.com>
2024-12-23 12:11:04 -0500

    Add project 1 text
    12/23/2024

--- src/Project 1/Project1_App.jsx
+++ src/Project 1/Project1_App.jsx
@@ -2,13 +2,18 @@ import React, { Component } from "react";
 import { useState } from "react";
 import "./Project1_App.css";
 import Header from "../components/Header/header";
+import Project1_Body from "./components/Project1_Body";
 
 class Project1_App extends Component {
   render() {
     return (
-      <>
-        <Header page={"Project 1"} />
-      </>
+      <div>
+        <a>
+          <Header page={"Project 1"} />
+        </a>
+
+        <Project1_Body />
+      </div>
     );
   }
 }
---
+++ src/Project 1/components/Project1_Body.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Project1_Body.css";
+
+const Project1_Body = () => {
+  return <h1 className="position">Project 1 will go here</h1>;
+};
+
+export default Project1_Body;
