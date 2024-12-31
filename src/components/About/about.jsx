82345783038c7f7775459a9cd8afe142a927dc0c
WilliamPottey <wottey9007@gmail.com>
2024-12-31 10:16:28 -0500

    adjusted scroll for about section. New Style for H1

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -12,68 +12,68 @@ import reactLogo from "./images/react_logo.png";
 const about = () => {
   return (
     <div className="background">
-      <span className="about">
-        <h1 className="display-5 fw-bold mb-5 mt-4" id="whiteText">
-          About Me
-        </h1>
-        <div className="about-flex-col">
-          <img className="myFace" src={myLogo} />
-          <div className="about-flex-row">
-            <div className="aboutDesc" id="whiteText">
-              For as long as I can remember, I've been passionate about
-              computers and Software Development. I graduated from the
-              University of Massachusetts, Amherst with a B.S. in Computational
-              Mathematics. I have been writing code since 2018, and have been
-              professionally developing since 2022. I have experience with Java,
-              Python, JavaScript, CSS, HTML, and PostgreSQL, along with
-              Proprietary Programming Languages I've used in my professional
-              work.
-            </div>
-            <div id="code-logos">
-              <img
-                id="java-logo"
-                className="code-logo-1 logo-animation-1"
-                src={javaLogo}
-                title="Java"
-              />
-              <img
-                id="java-logo"
-                className="code-logo-1 logo-animation-1"
-                src={pythonLogo}
-                title="Python"
-              />
-              {/* <img id="js-logo" className="code-logo" src={jsLogo} />
+      <div className="page-margin-top">
+        <h1 className="display-5 fw-bold mb-3 about-header">About Me</h1>
+        <span className="about">
+          <div className="about-flex-col">
+            <img className="myFace" src={myLogo} />
+            <div className="about-flex-row">
+              <div className="aboutDesc">
+                For as long as I can remember, I've been passionate about
+                computers and Software Development. I graduated from the
+                University of Massachusetts, Amherst with a B.S. in
+                Computational Mathematics. I have been writing code since 2018,
+                and have been professionally developing since 2022. I have
+                experience with Java, Python, JavaScript, CSS, HTML, and
+                PostgreSQL, along with Proprietary Programming Languages I've
+                used in my professional work.
+              </div>
+              <div id="code-logos">
+                <img
+                  id="java-logo"
+                  className="code-logo-1 logo-animation-1"
+                  src={javaLogo}
+                  title="Java"
+                />
+                <img
+                  id="java-logo"
+                  className="code-logo-1 logo-animation-1"
+                  src={pythonLogo}
+                  title="Python"
+                />
+                {/* <img id="js-logo" className="code-logo" src={jsLogo} />
               <img className="code-logo" src={cssLogo} /> */}
-              {/* <img id="jch-logos" src={testImage} /> */}
-              <img
-                className="code-logo-2 logo-animation-1"
-                src={cssLogo}
-                title="CSS"
-              />
-              <img
-                className="code-logo-2 logo-animation-1"
-                src={htmlLogo}
-                title="HTML"
-              />
-              <img
-                className="code-logo-2 logo-animation-1"
-                src={jsLogo}
-                title="JavaScript"
-              />
-              <img
-                className="code-logo-1 logo-animation-1"
-                src={postgresLogo}
-                title="PostgreSQL"
-              />
-              <img
-                className="code-logo-1 logo-animation-1"
-                src={reactLogo}
-                title="React.js"
-              />
+                {/* <img id="jch-logos" src={testImage} /> */}
+                <img
+                  className="code-logo-2 logo-animation-1"
+                  src={cssLogo}
+                  title="CSS"
+                />
+                <img
+                  className="code-logo-2 logo-animation-1"
+                  src={htmlLogo}
+                  title="HTML"
+                />
+                <img
+                  className="code-logo-2 logo-animation-1"
+                  src={jsLogo}
+                  title="JavaScript"
+                />
+                <img
+                  className="code-logo-1 logo-animation-1"
+                  src={postgresLogo}
+                  title="PostgreSQL"
+                />
+                <img
+                  className="code-logo-1 logo-animation-1"
+                  src={reactLogo}
+                  title="React.js"
+                />
+              </div>
             </div>
           </div>
-        </div>
-      </span>
+        </span>
+      </div>
       <hr />
     </div>
   );
